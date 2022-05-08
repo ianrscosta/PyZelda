f09606b87e69f0ce5fa513703a92eadea9ba878e
ianrscosta <ianrcosta@gmail.com>
2022-05-08 16:50:39 -0300

    updating the tiles groups

--- Code/level.py
+++ Code/level.py
@@ -13,7 +13,7 @@ class Level:
 
         # sprite group setup
         self.visible_sprites = pygame.sprite.Group()
-        self.obstacles_sprites = pygame.sprite.Group()
+        self.obstacle_sprites = pygame.sprite.Group()
 
         # sprite setup
         self.create_map()
@@ -25,7 +25,7 @@ class Level:
                 y = row_index * TILESIZE
 
                 if col == 'x':
-                    Tile((x, y), [self.visible_sprites])
+                    Tile((x, y), [self.visible_sprites, self.obstacle_sprites])
 
                 if col == 'p':
                     Player((x, y), [self.visible_sprites])
