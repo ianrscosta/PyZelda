a2e3877546e3f27f736585cd45cbd465ce19ae85
ianrscosta <ianrcosta@gmail.com>
2022-05-08 17:26:09 -0300

    player basic movements

--- Code/tile.py
+++ Code/tile.py
@@ -5,5 +5,5 @@ from settings import *
 class Tile(pygame.sprite.Sprite):
     def __init__(self, pos, groups):
         super().__init__(groups)
-        self.image = pygame.image.load('../Graphics/rock.png').convert_alpha()
+        self.image = pygame.image.load('./Graphics/rock.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
